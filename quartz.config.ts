3e8b23728998c98b90ffc94242d47b5c27ef1aab
MPzinkk <matheusps2015@gmail.com>
2026-04-02 14:41:43 -0300

    Quartz sync: Apr 2, 2026, 2:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Cinzel",
+        body: "Crimson Text",
         code: "IBM Plex Mono",
       },
       colors: {
